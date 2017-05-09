5e0e0c0f02702c2ad1a3a87d4e84d0fb2197c5ae
Harshitha Gandamalla <harsh1thagandama@ufl.edu>
2017-05-09 09:51:48 -0400

    Angular and bootstrap initial setup

--- routes/tasks.js
+++ routes/tasks.js
@@ -33,4 +33,69 @@ router.get('/tasks/:id',function(req,res,next){
 });
 
 
+router.post('/task',function(req,res,next){
+   var task=req.body;
+   if(!task.title || (task.isDone+' ')){
+     res.status(400);
+     res.json({
+       "error":"Bad Data"
+     });
+
+
+   }else{
+     db.tasks.save(task,function(err,task){
+         if(err){
+           res.send(err);
+         }
+         res.json(task);
+
+     });
+   }
+
+});
+
+//Delete Task
+router.delete('/tasks/:id',function(req,res,next){
+//  res.send('TASK API');
+ db.tasks.remove({_id: mongojs.ObjectId(req.params.id)},function(err,task){
+
+   if(err){
+    res.send(err);
+  }
+   res.json(task);
+
+
+ });
+});
+
+//Update Task
+router.put('/tasks/:id',function(req,res,next){
+
+  var task=req.body;
+  var updTask={};
+
+  if(task.isDone){
+    updTask.isDone=task.isDone;
+  }
+  if(task.title){
+    updTask.isDone=task.title;
+  }
+  if(!updTask){
+    res.status(400);
+    res.json({"error":"Bad Data"});
+  }
+  else{
+    db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask,{},function(err,task){
+
+      if(err){
+       res.send(err);
+     }
+      res.json(task);
+
+
+    });
+  }
+
+});
+
 module.exports=router;
